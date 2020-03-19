a84c3b799db45cf270d076b7d523a13ec2e9d24a
Nitin Dhameja <nitin.dhameja@gmail.com>
2020-03-19 10:27:03 -0700

    Create 1119_eazt.java

---
+++ 1119_eazt.java
@@ -0,0 +1,31 @@
+/*
+1119. Remove Vowels from a String
+Given a string S, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+
+Example 1:
+
+Input: "leetcodeisacommunityforcoders"
+Output: "ltcdscmmntyfrcdrs"
+Example 2:
+
+Input: "aeiou"
+Output: ""
+ 
+
+Note:
+
+S consists of lowercase English letters only.
+1 <= S.length <= 1000
+*/
+
+var removeVowels = function(S) {
+    var x = [];
+    var set = new Set(['a','e','i','o','u']);
+    
+    for (var i = 0 ;i < S.length;i++){
+        if (!set.has(S[i])){
+            x.push(S[i]);
+        }      
+    } 
+    return x.join('');
+};
